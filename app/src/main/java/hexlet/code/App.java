c42c3f9ea0bb806855ca5108f6411670c420ab51
Phosphorusss <phosphorus959@gmail.com>
2023-12-21 13:33:15 +0300

    modified .gitignore build.gradle App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Callable<Integer> {
+    @Parameters(index = "0", paramLabel="filepath1", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", paramLabel="filepath2", description = "path to second file")
+    private String filepath2;
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    boolean usageHelp;
+    @Option(names = {"-v", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean usageVersion;
+    @Option(names = {"-f", "--format"},description = "output format [default: stylish]")
+    String[] format;
+    @Override
+    public Integer call() throws Exception {
+        System.out.println(Differ.generate(filepath1, filepath2));
+        return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.exit(new CommandLine(new App()).execute(args));
     }
 }
